f1c78f834d5bd1c8f0ba28525db6cc550d4d8a00
invalidusername42069 <sin11907@shernet.sheridancollege.on.ca>
2022-05-25 10:04:48 -0400

    version 1 code by Harmanpreet Singh

---
+++ src/studentdetails/StudentDetails.java
@@ -0,0 +1,31 @@
+package studentdetails;
+import java.util.Scanner;
+
+public class StudentDetails {
+
+    public static void main(String[] args) {
+        students[] studentlist = new students[3];
+        
+        System.out.println("Enter student name ");
+        
+        students s1 = new students();
+        students s2 = new students();
+        students s3 = new students();
+        s1.setName("Dixie normous");
+        s1.setAge(69);
+        s2.setName("Hugh Jass");
+        s2.setAge(420);
+        s3.setName("Same En");
+        s3.setAge(42069);
+        //Storing the values in the array
+        studentlist[0]=s1;
+        studentlist[1]=s2;
+        studentlist[2]=s3;
+        
+        for(int i = 0;i<studentlist.length;i++){
+        System.out.println(studentlist[i].getName()+" "+studentlist[i].getAge());
+        }
+        
+    }
+    
+}
---
+++ src/studentdetails/students.java
@@ -0,0 +1,41 @@
+package studentdetails;
+/** raw data of students, model class
+ * student name,age etc
+ * @author asus
+ */
+ 
+
+public class students {
+    private String name;
+    private int age;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
